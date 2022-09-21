fa3bd517d75656d0d68692490fa5ce6276ee6d53
Yusef <yusefzarzar@gmail.com>
2022-09-21 20:19:37 +0300

    added search screen functionality

--- src/screens/studentSearchPage.jsx
+++ src/screens/studentSearchPage.jsx
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
+import { useState } from 'react';
 import "../styles/studentSearchPage.css";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from '../App';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function StudentSearchPage() {
+
+    const navigate = useNavigate();
+
+    const handleSearch = async () => {
+
+        const docRef = doc(db, "children", studentID);
+        const docSnap = await getDoc(docRef);
+        
+        if (docSnap.exists()) {
+            //edit
+            navigate("/choice");
+          console.log("Document data:", docSnap.data());
+        } else {
+          setErrorMessage("Student doesn't exist on our database");
+        }
+    }
+
+
+    const [studentID, setStudentID] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
     return ( 
         <div className="studentSearchPageContainer">
             <img src={require("../assets/studentSearchUpperBgPart.png")} alt="" className='studentSearchUpperBgPart'/>
             <img src={require("../assets/studentSearchLowerBgPart.png")} alt="" className='studentSearchLowerBgPart'/>
             <div className="formContentContainer">
                 <img src={require("../assets/studentSearchWelcomingImage.png")} alt="" className='welcomingImage'/>
-                <input type="email" name="email" id="email" className='FormInputField' placeholder='Enter the student ID'/>
-                <input type="button" value="Search" className='searchButton'/>
-
+                <input type="text" name="studentID" id="studentID" className='FormInputField' placeholder='Enter the student ID' onChange={(event) => {
+                    setStudentID(event.target.value);
+                    setErrorMessage("");
+                }}/>
+                <input type="button" value="Search" onClick={() => {
+                    handleSearch().then((promise) => {
+                        console.log("success")
+                    }).catch((error) => {
+                        console.log("error")
+                    })
+                }} className='searchButton'/>
+                {errorMessage !== ""? <p className='errorMessage'>{errorMessage}</p>: null}
             </div>
         </div>
      );
