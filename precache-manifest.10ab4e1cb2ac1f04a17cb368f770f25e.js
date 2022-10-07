33ddb43855bf42cefc2ddb0db45945d2c73783ba
Beesan-AOU <beesan.aou@gmail.com>
2022-10-07 16:26:57 +0300

    Updates

---
+++ precache-manifest.10ab4e1cb2ac1f04a17cb368f770f25e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "76fe58673c157e246329",
+    "url": "/E-Wallet/static/css/main.1cdbe505.chunk.css"
+  },
+  {
+    "revision": "76fe58673c157e246329",
+    "url": "/E-Wallet/static/js/main.76fe5867.chunk.js"
+  },
+  {
+    "revision": "083a92f3bb60d465e079",
+    "url": "/E-Wallet/static/js/1.083a92f3.chunk.js"
+  },
+  {
+    "revision": "b1ddbce7a1c1ad97ac82",
+    "url": "/E-Wallet/static/css/2.f100d079.chunk.css"
+  },
+  {
+    "revision": "b1ddbce7a1c1ad97ac82",
+    "url": "/E-Wallet/static/js/2.b1ddbce7.chunk.js"
+  },
+  {
+    "revision": "fdcb83065a0d4c477634",
+    "url": "/E-Wallet/static/js/runtime~main.fdcb8306.js"
+  },
+  {
+    "revision": "84d9b6c866d51466b2091d45b8b1139f",
+    "url": "/E-Wallet/static/media/default-boy-image.84d9b6c8.png"
+  },
+  {
+    "revision": "a7f53b21e7902e7d0fb06fb58f131b9b",
+    "url": "/E-Wallet/static/media/default-girl-image.a7f53b21.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/E-Wallet/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9fb897254341cb38e7f9bac3b93d6bb6",
+    "url": "/E-Wallet/static/media/landingBackgroundCircle.9fb89725.png"
+  },
+  {
+    "revision": "73a5e9b8dae1a4fea5986e6cb8502a59",
+    "url": "/E-Wallet/static/media/upperCircleImage.73a5e9b8.png"
+  },
+  {
+    "revision": "1a04fa4f19bbc0686fbfe4c467551ada",
+    "url": "/E-Wallet/static/media/lowerCircleImage.1a04fa4f.png"
+  },
+  {
+    "revision": "b2f1c7d4f6645c1f2082fca21dce7143",
+    "url": "/E-Wallet/static/media/laptopImage.b2f1c7d4.png"
+  },
+  {
+    "revision": "40446609e4ea1f1bb9fc5a982367cd0a",
+    "url": "/E-Wallet/static/media/parentChoiceImage.40446609.png"
+  },
+  {
+    "revision": "561b2a9e4a927a4b94ec26e1cb4a13a3",
+    "url": "/E-Wallet/static/media/schoolChoiceImage.561b2a9e.png"
+  },
+  {
+    "revision": "f72446953478ec2a6547d570bedded2d",
+    "url": "/E-Wallet/static/media/calenderAdImage.f7244695.png"
+  },
+  {
+    "revision": "301e265c6c7d3c1a2f1a6f38a487c177",
+    "url": "/E-Wallet/static/media/mathAdImage.301e265c.png"
+  },
+  {
+    "revision": "07275cb8a33fe08c03efd0abbca229fe",
+    "url": "/E-Wallet/static/media/parentsSampleImage.07275cb8.png"
+  },
+  {
+    "revision": "d63813a6d5cfb00f72adfe5b2ef1f5e3",
+    "url": "/E-Wallet/static/media/ParentHomePageLowerBgPart.d63813a6.png"
+  },
+  {
+    "revision": "d0d01a92a3f73eb146f54008adcc5bdc",
+    "url": "/E-Wallet/static/media/instagramLogo.d0d01a92.png"
+  },
+  {
+    "revision": "570b9c19f0a0e95350470f972a66abb5",
+    "url": "/E-Wallet/static/media/twitterLogo.570b9c19.png"
+  },
+  {
+    "revision": "957636570295f401ef24e329de8de346",
+    "url": "/E-Wallet/static/media/loginPageLeftBgPart.95763657.png"
+  },
+  {
+    "revision": "980f42819e39f5534a780c77ffd6acf1",
+    "url": "/E-Wallet/static/media/studentSearchWelcomingImage.980f4281.png"
+  },
+  {
+    "revision": "17f0097e2bb2105727b2430ff71f3320",
+    "url": "/E-Wallet/static/media/popupSearchApprpvalPageLeftBgPart.17f0097e.png"
+  },
+  {
+    "revision": "69df5e990734b0d0482809b2ce6831b7",
+    "url": "/E-Wallet/index.html"
+  }
+];
